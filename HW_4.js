d02bb9f2e11a24f49599e237912ee6d2c55e620a
TextByVera <vera.kologrivova@gmail.com>
2020-06-05 02:38:17 +0200

    ДЗ 4

---
+++ HW_4.js
@@ -0,0 +1,7 @@
+
+
+const MINUTES_IN_HOUR = 60;
+
+const bussesPasses = (23-6)*MINUTES_IN_HOUR/20; 
+console.log('Сколько раз автобус будет стоять на конечной остановке?');
+console.log(bussesPasses);
