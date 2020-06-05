88eba4df5a8b712e1c0f04eadc32b154cf22ff08
TextByVera <vera.kologrivova@gmail.com>
2020-06-05 02:46:40 +0200

    ДЗ 5

---
+++ HW_5.js
@@ -0,0 +1,7 @@
+
+const MINUTES_IN_HOUR = 60;
+const SECONDS_IN_MINUTE = 60;
+
+const secondsFromMidnight = (13*MINUTES_IN_HOUR+45)*SECONDS_IN_MINUTE;
+console.log('Сколько прошло секунд с 00:00 до 13:45?');
+console.log(secondsFromMidnight);
