f4232bafd9e58346a193ab83a0e5a7369146f031
TextByVera <vera.kologrivova@gmail.com>
2020-06-11 22:52:07 +0200

    ДЗ 13

---
+++ HW_13.js
@@ -0,0 +1,17 @@
+//Выведете в консоль элементы массива с помощью цикла for
+
+console.log ("С помощью цикла FOR:");
+
+const names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+for (let i = 0; i < names.length; i = i + 1) {
+    console.log (`${i+1} элемент массива - ${names[i]}`);
+}
+
+//Сделайте то же самое, с помощью метода forEach
+
+console.log ("\nС помощью метода forEach:");
+
+names.forEach(function(item, index) {
+    console.log (`${item} имеет позицию ${index}`);
+  });
