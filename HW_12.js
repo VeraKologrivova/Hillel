3023878a643b5ea18a05bd06f48c82981e87a026
VeraKologrivova <63669264+VeraKologrivova@users.noreply.github.com>
2020-06-11 23:18:53 +0300

    HW_12 (update)
    скобку в коде случайно перенесла на новую строку, исправила

--- HW_12.js
+++ HW_12.js
@@ -3,8 +3,7 @@ const originalString = prompt('Enter the string');
 function removeVowels(str) {
     let newStr = '';
    for (let i = 0; i < str.length; i = i + 1) {
-       if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] !='u' && str[i] != 'y')
-       {
+       if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] !='u' && str[i] != 'y') {
            newStr = newStr + str[i];
        }
    }
